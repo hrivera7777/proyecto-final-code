0a8179f46a07a3eae2d668ea9d5920869493f2d6
Hans Rivera <hrivera@unal.edu.co>
2021-09-01 20:12:17 -0500

    created controller and view to show all suplyInventory

--- app/Http/Controllers/RetiroInventario.php
+++ app/Http/Controllers/RetiroInventario.php
@@ -4,13 +4,22 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\MateriaPrima\InventarioSuministro;
+use App\MateriaPrima\Suministro;
 
 class RetiroInventario extends Controller
 {
     public function show() {
         $inventario = InventarioSuministro::all();
+        $suministrosCompletos = [];
+
+        foreach ($inventario as $inv) {
+            
+            $suministro =Suministro::find($inv->id_suministro);
+            array_push($suministrosCompletos,$suministro);
+        }
         
-        return view('retiroInventario',['datosInv',$inventario]);
+        //dd($inventarioCompleto);           
+        return view('retiroInventario',['datosInv'=>$inventario,'datosSumi'=>$suministrosCompletos]);
     }
 
     public function retirar(Request $request){
--- resources/views/retiroInventario.blade.php
+++ resources/views/retiroInventario.blade.php
@@ -1,8 +1,55 @@
 @extends('layout')
 
 @section('content')
-<div class="container-fluid px-4">
-    <h1 class="mt-4">Retiro de suministros del inventario</h1>
-    <h1>{{$inventario}}</h1>
-</div>
+    <div class="container-fluid px-4">
+        <h1 class="mt-4">Retiro de suministros del inventario</h1>
+        
+        <div class="card mb-4">
+            <div class="card-header">
+                <i class="fas fa-table me-1"></i>
+                DataTable Example
+            </div>
+            <div class="card-body">
+                <table id="datatablesSimple">
+                    
+                    <thead>
+                        <tr>
+                            <th>id</th>
+                            <th>cantidades</th>
+                            <th>id pedido</th>
+                            <th>id suministro</th>
+                            <th>tipo suministro</th>
+                            <th>peso suministro</th>
+                            <th>fecha vencimiento</th>
+                            <th>disponible</th>
+                            <th>unidades</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @for ($i = 0; $i < count($datosInv); $i++)
+                            <tr>
+                                <td>{{$datosInv[$i]->id}}</td>
+                                <td>{{$datosInv[$i]->cantidades}}</td>
+                                <td>{{$datosInv[$i]->id_pedido}}</td>
+                                <td>{{$datosSumi[$i]->id}}</td>
+                                <td>{{$datosSumi[$i]->tipo_suministro}}</td>
+                                <td>{{$datosSumi[$i]->peso}}</td>
+                                <td>{{$datosSumi[$i]->fecha_vencimiento}}</td>
+                                <td>{{$datosSumi[$i]->disponible}}</td>
+                                <td>{{$datosSumi[$i]->unidades}}</td>
+                            </tr>
+                            
+                        @endfor
+                        {{-- @foreach ($datosInv as $inventario)
+                        @endforeach
+                        @foreach ($datosSumi as $sumi)
+                        <tr>
+                        </tr>
+                        @endforeach --}}
+                            
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
 @endsection
